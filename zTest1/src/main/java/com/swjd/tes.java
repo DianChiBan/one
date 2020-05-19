4ed0d73ae80f1aca02a0621de027ab9e439741b2
admin <1541036161@qq.com>
2020-05-19 16:05:07 +0800

    idea提交测试

---
+++ zTest1/src/main/java/com/swjd/tes.java
@@ -0,0 +1,29 @@
+package com.swjd;
+
+public class tes {
+    public static void main(String[] args) {
+        Integer id = test.id=1;
+        int no = new test().no=4;
+        System.out.println("id1==5==>"+id);
+        System.out.println("no1==10==>"+no);
+        new tes().st();
+        n();
+    }
+
+    public void st(){
+        sc();
+        Integer id = test.id;
+        int no = new test().no;
+        System.out.println("id==>"+id);
+        System.out.println("no==>"+no);
+    }
+
+
+    public static void sc(){
+        System.out.println("sc()的方法被调用了");
+    }
+
+    public static void n(){
+        sc();
+    }
+}
